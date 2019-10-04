db1be96447c26eddf36213bcd47ff6c8c81654c4
Hamel Husain <hamel.husain@gmail.com>
2019-10-03 17:53:08 -0700

    Update wandb_get_runs.py

--- wandb_get_runs.py
+++ wandb_get_runs.py
@@ -8,6 +8,7 @@ The purpose is to compare runs from a given SHA to runs you may have tagged as b
 
 
 import os
+os.environ["WANDB_API_KEY"] = os.getenv('INPUT_WANDB_API_KEY')
 import wandb
 import logging
 import pandas as pd
